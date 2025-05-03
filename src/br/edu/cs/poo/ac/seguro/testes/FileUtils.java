e880b5bde5911fbbf191dbb2266f5c6f3b3bdcb2
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-03 00:19:02 -0300

    Update FileUtils.java

--- src/br/edu/cs/poo/ac/seguro/testes/FileUtils.java
+++ src/br/edu/cs/poo/ac/seguro/testes/FileUtils.java
@@ -1,1 +1,20 @@
+package br.edu.cs.poo.ac.seguro.testes;
+
+import java.io.File;
+
+public class FileUtils {
+    public static void limparDiretorio(String caminho) {
+        File dir = new File(caminho);
+        if (dir.exists() && dir.isDirectory()) {
+            File[] arquivos = dir.listFiles();
+            if (arquivos != null) {
+                for (File arquivo : arquivos) {
+                    if (arquivo != null && arquivo.isFile()) {
+                        arquivo.delete();
+                    }
+                }
+            }
+        }
+    }
+}
 
