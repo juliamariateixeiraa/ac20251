15521f720673e39d338475720e89e9fc9539c4e1
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-03 00:13:36 -0300

    Update TesteMediator.java

--- src/br/edu/cs/poo/ac/seguro/testes/TesteMediator.java
+++ src/br/edu/cs/poo/ac/seguro/testes/TesteMediator.java
@@ -1,1 +1,7 @@
+package br.edu.cs.poo.ac.seguro.testes;
 
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+
+public abstract class TesteMediator extends TesteAbstrato {
+	
+}
