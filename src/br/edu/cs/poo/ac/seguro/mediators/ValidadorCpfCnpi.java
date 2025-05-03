9f512488e1d450c35f5f268368acc36587d47260
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-02 22:08:40 -0300

    Update ValidadorCpfCnpi.java

--- src/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpi.java
+++ src/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpi.java
@@ -1,4 +1,80 @@
-package br.edu.cs.poo.ac.seguro.mediators;
+package br.edu.cs.poo.ac.seguro.util;
 
-public class ValidadorCpfCnpi {
+public class ValidadorCpfCnpj {
+
+    private ValidadorCpfCnpj() {
+        // Construtor privado para evitar instanciação
+    }
+
+    /**
+     * Valida se um CPF é válido.
+     *
+     * @param cpf o CPF a ser validado (apenas números, sem pontos ou traços)
+     * @return true se o CPF for válido, false caso contrário
+     */
+    public static boolean ehCpfValido(String cpf) {
+        if (cpf == null) return false;
+
+        cpf = cpf.replaceAll("\\D", ""); // Remove caracteres não numéricos
+
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) return false;
+
+        try {
+            int soma = 0, peso = 10;
+            for (int i = 0; i < 9; i++) {
+                soma += (cpf.charAt(i) - '0') * peso--;
+            }
+            int digito1 = 11 - (soma % 11);
+            digito1 = (digito1 > 9) ? 0 : digito1;
+
+            soma = 0;
+            peso = 11;
+            for (int i = 0; i < 10; i++) {
+                soma += (cpf.charAt(i) - '0') * peso--;
+            }
+            int digito2 = 11 - (soma % 11);
+            digito2 = (digito2 > 9) ? 0 : digito2;
+
+            return cpf.charAt(9) - '0' == digito1 && cpf.charAt(10) - '0' == digito2;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Valida se um CNPJ é válido.
+     *
+     * @param cnpj o CNPJ a ser validado (apenas números, sem pontos, traços ou barras)
+     * @return true se o CNPJ for válido, false caso contrário
+     */
+    public static boolean ehCnpjValido(String cnpj) {
+        if (cnpj == null) return false;
+
+        cnpj = cnpj.replaceAll("\\D", ""); // Remove caracteres não numéricos
+
+        if (cnpj.length() != 14 || cnpj.matches("(\\d)\\1{13}")) return false;
+
+        try {
+            int[] pesos1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+            int[] pesos2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+            int soma = 0;
+            for (int i = 0; i < 12; i++) {
+                soma += (cnpj.charAt(i) - '0') * pesos1[i];
+            }
+            int digito1 = soma % 11;
+            digito1 = (digito1 < 2) ? 0 : 11 - digito1;
+
+            soma = 0;
+            for (int i = 0; i < 13; i++) {
+                soma += (cnpj.charAt(i) - '0') * pesos2[i];
+            }
+            int digito2 = soma % 11;
+            digito2 = (digito2 < 2) ? 0 : 11 - digito2;
+
+            return cnpj.charAt(12) - '0' == digito1 && cnpj.charAt(13) - '0' == digito2;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
