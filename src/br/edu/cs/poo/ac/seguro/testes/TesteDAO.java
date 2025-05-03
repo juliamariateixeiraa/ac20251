04d82a231086c88a0f165ac746387556da792e8b
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-03 00:14:57 -0300

    Update TesteDAO.java

--- src/br/edu/cs/poo/ac/seguro/testes/TesteDAO.java
+++ src/br/edu/cs/poo/ac/seguro/testes/TesteDAO.java
@@ -1,29 +1,33 @@
 package br.edu.cs.poo.ac.seguro.testes;
 
 import java.io.File;
-
 import org.junit.jupiter.api.BeforeEach;
 
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
-import br.edu.cs.poo.ac.seguro.daos.SeguradoEmpresaDAO;
-import br.edu.cs.poo.ac.seguro.entidades.SeguradoEmpresa;
 
-public abstract class TesteDAO {
+public abstract class TesteDAO extends TesteAbstrato {
     protected CadastroObjetos cadastro;
-
-    protected abstract Class getClasse();
-
+    
+    protected abstract Class<?> getClasse();
+    
     protected TesteDAO() {
         cadastro = new CadastroObjetos(getClasse());
     }
-
+    
     @BeforeEach
     public void setUp() {
         String sep = File.separator;
         File dir = new File("." + sep + getClasse().getSimpleName());
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
         File[] files = dir.listFiles();
-        for (File file : files) {
-            file.delete();
+        if (files != null) {
+            for (File file : files) {
+                if (file != null) file.delete();
+            }
         }
     }
 }
