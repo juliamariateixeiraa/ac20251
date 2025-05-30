fe9e2ec991a3f81d316cc7cadd4a4b2b7bfe991d
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-29 21:48:54 -0300

    Update Veiculo.java

--- src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -7,12 +7,17 @@ import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-public class Veiculo implements Serializable {
+public class Veiculo implements Registro {
+
     private static final long serialVersionUID = 1L;
 
     private String placa;
     private int ano;
-    private SeguradoEmpresa proprietarioEmpresa;
-    private SeguradoPessoa proprietarioPessoa;
+    private Segurado proprietario;
     private CategoriaVeiculo categoria;
+
+    @Override
+    public String getIdUnico() {
+        return placa;
+    }
 }
