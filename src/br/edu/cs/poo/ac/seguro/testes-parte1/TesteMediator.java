0aac76176aa91f98e82c81ffa9bb7405a3f6eaa8
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-29 22:02:43 -0300

    TESTES FUNCIONANDO

--- src/br/edu/cs/poo/ac/seguro/testes-parte1/TesteMediator.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cs.poo.ac.seguro.testes;
-
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
-
-public abstract class TesteMediator extends TesteAbstrato {
-
-
-}
